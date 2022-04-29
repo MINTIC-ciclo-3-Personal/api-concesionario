4854e59266ba9606c796da5580c92a271d9819c3
[Nikion] <[nigutjim@gmail.com]>
2022-04-29 13:00:14 -0500

    agregar archivo para manejar la conexion a la base de datos

---
+++ db/db.js
@@ -0,0 +1,31 @@
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env' });
+
+const strigConexion = process.env.DAtABASE_URL;
+
+const client = new MongoClient(strigConexion, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+let conexion;
+
+const conectarBD = (callback) => {
+    client.connect((err, db) => {
+        if (err) {
+            console.error('Error conectando a la base de datos');
+            return false;
+        }
+        conexion = db.db('concesionario');
+        console.log('Conexion exitosa');
+        return callback
+    });
+}
+
+const getBD =() => {
+    return conexion;
+}
+
+export { conectarBD, getBD };
